ebd8cff89eaeae368ec1c2e262bfa1efdd64a3ec
Gustavo Bazzo <gcbazzo@gmail.com>
2022-11-03 16:13:36 -0400

    Update version.php

--- version.php
+++ version.php
@@ -13,11 +13,10 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
  * *************************************************************************
  * ************************************************************************ */
-$plugin->version = 2022020900;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version = 2022100100;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2020061500.00; // Moodle 3.9.0
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = 'v1.15.0';
-$plugin->cron = 0;           // Period for cron to check this module (secs)
+$plugin->release = 'v1.16.0-beta';
 $plugin->component = 'mod_tab';
 $plugin->supported = [39, 400];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
-$plugin->dependencies = [];
+
