0ee28c24750f40a90df737805f911a6d59895e39
Gustavo Bazzo <gcbazzo@gmail.com>
2023-06-01 13:59:41 -0400

    Update version.php

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 $plugin->version = 2023011900;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2020061500.00; // Moodle 3.9.0
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.16.1-stable';
+$plugin->release = 'v1.16.2-stable';
 $plugin->component = 'mod_tab';
 $plugin->supported = [39, 410];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 
